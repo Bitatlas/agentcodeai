c069bde52a0259c4efdd2615f7ffb44b5db5d1d7
Henrique Centieiro <32439646+Bitatlas@users.noreply.github.com>
2024-11-29 12:06:38 +0800

    Update page.tsx

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function Page() {
-  const gameState = await getGameState();ssds876543
+  const gameState = await getGameState();
 
   return <Faq gameState={gameState} />;
 }
